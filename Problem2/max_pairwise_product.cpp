bae9ec10dff40a7e5a879ecb58975aec2a3b9d1f
Brian Gudely <bcgudely@gmail.com>
2017-01-28 16:37:17 -0800

    added faster algorithm for problem 2 c++

--- Problem2/max_pairwise_product.cpp
+++ Problem2/max_pairwise_product.cpp
@@ -5,6 +5,22 @@ using std::vector;
 using std::cin;
 using std::cout;
 
+long long MaxPairwiseProductFast(const vector<int>& numbers) {
+  int n = numbers.size();
+
+  int max_index1 = -1;
+  for (int i = 0; i < n; ++i)
+    if ((max_index1 == -1) || (numbers[i] > numbers[max_index1]))
+      max_index1 = i;
+
+  int max_index2 = -1;
+  for (int j = 0; j < n; ++j)
+    if ((numbers[j] != numbers[max_index1]) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+      max_index2 = j;
+
+  return ((long long)(numbers[max_index1])) * numbers[max_index2];
+}
+
 long long MaxPairwiseProduct(const vector<int>& numbers) {
   long long result = 0;
   int n = numbers.size();
@@ -19,14 +35,15 @@ long long MaxPairwiseProduct(const vector<int>& numbers) {
 }
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> numbers(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> numbers[i];
-    } 
+  int n;
+  cin >> n;
+  vector<int> numbers(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> numbers[i];  
+  }
 
-    long long result = MaxPairwiseProduct(numbers);
-    cout << result << "\n";
-    return 0;
+  long long result1 = MaxPairwiseProduct(numbers);
+  long long result2 = MaxPairwiseProductFast(numbers);
+  cout << result1 << "\n" << result2;
+  return 0;
 }
