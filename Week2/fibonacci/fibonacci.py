9bef86ec60359968d0d3f6179809ec41cc81d444
Brian Gudely <bcgudely@gmail.com>
2017-01-30 11:49:16 -0800

    1st fibonacci is now working correctly

--- Week2/fibonacci/fibonacci.py
+++ Week2/fibonacci/fibonacci.py
@@ -1,9 +1,10 @@
 # Uses python3
 def calc_fib(n):
-    if (n <= 1):
-        return n
+    numbers = [0, 1]
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    for i in range(2, n+1):
+        numbers.append(numbers[i-1] + numbers[i-2])
 
+    return numbers[n]
 n = int(input())
 print(calc_fib(n))
