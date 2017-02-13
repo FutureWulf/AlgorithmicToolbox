1e21d9bc636d1e0b27faa9660fd8e4edfab40894
Brian Gudely <bcgudely@gmail.com>
2017-02-13 07:59:54 -0800

    fractional knapsack done

--- Week3/fractional_knapsack/fractional_knapsack.py
+++ Week3/fractional_knapsack/fractional_knapsack.py
@@ -2,8 +2,26 @@
 import sys
 
 def get_optimal_value(capacity, weights, values):
-    value = 0.
+    value = 0
     # write your code here
+    fractions = []
+
+    for i in range(0, n):
+        fractions.append(values[i]/weights[i])
+
+    while capacity != 0 and len(fractions) > 0:
+        best_item = fractions.index(max(fractions))
+
+        if capacity >= weights[best_item]:
+            value += values[best_item]
+            capacity -= weights[best_item]
+
+            weights.pop(best_item)
+            values.pop(best_item)
+            fractions.pop(best_item)
+        else:
+            value += (capacity * fractions[best_item])
+            capacity = 0
 
     return value
 
