0923550e5627bb71c295fa75d03e2c62489beea5
Brian Gudely <bcgudely@gmail.com>
2017-02-19 18:46:22 -0800

    majority element is done

--- Week4/majority_element/majority_element.py
+++ Week4/majority_element/majority_element.py
@@ -1,18 +1,29 @@
 # Uses python3
 import sys
 
-def get_majority_element(a, left, right):
-    if left == right:
-        return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
-    return -1
+def get_majority_element(input_array, length):
 
+    occurances = {}
+    
+    for i in range(0, length):
+        key = input_array[i] 
+    
+        if key in occurances:
+            occurances[key] += 1
+            
+        else:
+            occurances[key] = 1
+    
+    return any(value > (length / 2) for value in occurances.values())
+   
+    
 if __name__ == '__main__':
+
     input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    length, *input_array = list(map(int, input.split()))
+    
+    if get_majority_element(input_array, length):
         print(1)
+
     else:
         print(0)
